4962b476d243c87756b7a29b91029e1fded0e480
luancntt2k600510 <ducluan05102001@gmail.com>
2023-07-01 00:56:51 +0700

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,6 @@ import PlacesPage from './pages/PlacesPage';
 import PlaceFormPage from './pages/PlaceFormPage';
 import PlacePage from './pages/PlacePage';
 import BookingsPage from './pages/BookingsPage';
-import BookingPage from './pages/BookingPage';
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
@@ -30,7 +29,6 @@ function App() {
           <Route path='/account/places/:id' element={<PlaceFormPage />}/>
           <Route path='/place/:id' element={<PlacePage />} />
           <Route path='/account/bookings' element={<BookingsPage />} />
-          <Route path='/account/bookings/:id' element={<BookingPage />} />
         </Route>
       </Routes>
     </UserContextProvider>
